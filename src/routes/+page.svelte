7abb8d107a1805554c30b2b605b29e20acd79925
CS <no@ne.please>
2023-04-18 12:26:36 +0200

    chore: Add WindiCSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import 'virtual:windi.css'
+</script>
+
+<h1 class="p-7">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), WindiCSS()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
