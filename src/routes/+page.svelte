62f6cbf1cd4a131672ef61609d9a8e5de3fbe764
CS <no@ne.please>
2023-04-25 02:26:42 +0200

    style: Add host/join buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,29 @@
-<script lang="ts">
-    import { goto } from "$app/navigation";
-</script>
-
-<div>
-    This is the main page.
+<div class="flex flex-col max-w-50 gap-3 absolute-center pb-8">
+    <button type="button">Host</button>
+    <button disabled type="button">Join</button>
 </div>
 
-<button on:click={() => goto("/profile")} class="bg-dark-400 text-white p-5">
-    Go to profile page
-</button>
+<style lang="scss">
+    // this CSS is scoped, so it's fine to target components directly
+    button {
+        background: #1d6589;
+        color: #fff;
+        padding: 1rem 5rem 1rem 5rem;
+        border-radius: 1rem;
+        font-size: large;
+        font-weight: 500;
+
+        &:disabled {
+            background: #ccc;
+            color: rgb(148, 148, 148);
+            cursor: not-allowed;
+        }
+    }
+
+    .absolute-center {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
--- src/routes/api/user/+server.ts
+++
@@ -1,6 +0,0 @@
-import type { RequestHandler } from "./$types";
-import { User } from "$lib/types/User";
-
-export const GET = (() => {
-	return new Response(JSON.stringify(new User("John")));
-}) satisfies RequestHandler;
