8723a900b7b7e535a5bfd0a4ff5cf229155682b2
CS <no@ne.please>
2023-04-25 16:17:52 +0200

    feat: Allow `server.py` to be ran without the Uvicorn CLI

--- python/server.py
+++ python/server.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from endpoints.host import host
 from endpoints.join import join
@@ -6,3 +7,7 @@ app = FastAPI()
 
 app.websocket("/host")(host)
 app.websocket("/join")(join)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
