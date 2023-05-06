46efc1b2cee623742f5072d6e5032baa23eee0d2
Mohammed Malloul <mohammed.malloul@hva.nl>
2023-05-06 14:33:00 +0200

    feat: added a .env file and changed localhost to env variables

--- src/routes/[pin]/+page.svelte
+++ src/routes/[pin]/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	import { onMount } from "svelte";
+	import { env } from "$env/static/public";
 
 	let status = "";
 	let messages: string[] = [];
 
 	onMount(() => {
 		const pin = $page.params.pin;
-		const websocket = new WebSocket(`ws://localhost:8000/join?pin=${pin}`);
+		const websocket = new WebSocket(`${env.WS_URL}/join?pin=${pin}`);
 		websocket.onclose = () => (status = "game not found");
 		websocket.onmessage = ({ data }) => {
 			if (status) {
