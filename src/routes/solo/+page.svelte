aab8d20e1a0e14b3f2bd2aa96e7395839e9ef044
Nb <slimmilk34@gmail.com>
2023-05-08 17:28:47 +0200

    refactor: delted code that was not needed.

--- src/routes/solo/+page.svelte
+++ src/routes/solo/+page.svelte
@@ -2,15 +2,14 @@
 	import { quintOut } from "svelte/easing";
 	import { crossfade } from "svelte/transition";
 	import { flip } from "svelte/animate";
-	import { env } from '$env/dynamic/public'
-	import { Configuration, OpenAIApi } from 'openai';
-	
-	// animation for choosing personality 
+	import { env } from "$env/dynamic/public";
+	import { Configuration, OpenAIApi } from "openai";
+
+	// animation for choosing personality
 	const [send, receive] = crossfade({
 		fallback(node, params) {
 			const style = getComputedStyle(node);
 			const transform = style.transform === "none" ? "" : style.transform;
-
 			return {
 				duration: 600,
 				easing: quintOut,
@@ -20,6 +19,21 @@
 			};
 		}
 	});
+
+	let visible = false;
+	function toggleVissible() {
+		visible = !visible;
+		const button = document.querySelector(".toggle-button");
+		if (visible) {
+			button.textContent = "Close";
+			document.querySelector(".absolute-center").style.top = "75%";
+		} else {
+			button.textContent = "Open";
+			document.querySelector(".absolute-center").style.top = "50%";
+		}
+	}
+
+	// Set traits
 	let traits = [
 		{ id: 1, done: false, description: "Funny" },
 		{ id: 2, done: false, description: "Scary" },
@@ -28,86 +42,74 @@
 		{ id: 5, done: false, description: "Clever" }
 	];
 
-	let visible = false;
-	function toggleVissible() {
-	visible = !visible
-		const button = document.querySelector('.toggle-button');
-		if (visible) {
-		button.textContent = 'Close';
-		document.querySelector('.absolute-center').style.top = '75%';
-		} else {
-		button.textContent = 'Open';
-		document.querySelector('.absolute-center').style.top = '50%';
-		}
-	}
-
 	// api request to openAI
 	let question = "";
-	let awnser:string = "";
+	let awnser: string = "";
 	async function openAiCall() {
 		const selectedTraits = traits.filter((t) => t.done).map((t) => t.description);
-		const selectedTraitsString = selectedTraits.join(', ');
+		const selectedTraitsString = selectedTraits.join(", ");
 		const configuration = new Configuration({
-			apiKey: env.PUBLIC_API_KEY_OPENAI,
+			apiKey: env.PUBLIC_API_KEY_OPENAI
 		});
-		const content = "you are a ouija response spirit that takes a question in and fills the blank or gives a awnser. Your personality is " + selectedTraitsString + " and the reponse must be really short 3 words max and if you cannot awnser anything because it is limiting you as a bot just say something funny.And stick to the personality traits given to you. "
-		
+		const content =
+			"you are a ouija response spirit that takes a question in and fills the blank or gives a awnser. Your personality is " +
+			selectedTraitsString +
+			" and the reponse must be really short 3 words max and if you cannot awnser anything because it is limiting you as a bot just say something funny.And stick to the personality traits given to you. ";
+
 		const openai = new OpenAIApi(configuration);
 		const completion = await openai.createChatCompletion({
 			model: "gpt-3.5-turbo",
-			messages: [{role: "system", content: content},
-					{role: "user", content: question}],			
+			messages: [
+				{ role: "system", content: content },
+				{ role: "user", content: question }
+			],
 			temperature: 0.7,
 			max_tokens: 80,
 			n: 1,
-			stop: '\n',
+			stop: "\n"
 		});
-		awnser = completion.data.choices[0].message?.content
-	};
-	
-
+		awnser = completion.data.choices[0].message?.content;
+	}
 </script>
-
 
 <div class="board">
 	<div class="awnser">
 		<h2><b class="awnser">SOLO Summon</b></h2>
-		<br>To select traits for the Ouija spirit, you can choose from a list of pre-defined traits
+		<br />To select traits for the Ouija spirit, you can choose from a list of pre-defined traits
 		that are relevant to the spirit you want to communicate with. The traits can be selected by
 		clicking on the corresponding checkboxes next to each trait. Once you have selected the desired
 		traits, you can click on the "summon" button to start the communication with the spirit.
-		<br>
-		<button class="awnser pd-5 toggle-button bg-indigo-800 text-white rounded px-10" on:click={toggleVissible}>
+		<br />
+		<button
+			class="awnser pd-5 toggle-button bg-indigo-800 text-white rounded px-10"
+			on:click={toggleVissible}
+		>
 			open
 		</button>
-	
 	</div>
-
-
 
 	{#if visible}
-	<div class="left">
-		<h2>Traits</h2>
-		{#each traits.filter((t) => !t.done) as todo (todo.id)}
-			<label in:receive={{ key: todo.id }} out:send={{ key: todo.id }} animate:flip>
-				<input type="checkbox" bind:checked={todo.done} />
-				{todo.description}
-			</label>
-		{/each}
-	</div>
+		<div class="left">
+			<h2>Traits</h2>
+			{#each traits.filter((t) => !t.done) as todo (todo.id)}
+				<label in:receive={{ key: todo.id }} out:send={{ key: todo.id }} animate:flip>
+					<input type="checkbox" bind:checked={todo.done} />
+					{todo.description}
+				</label>
+			{/each}
+		</div>
 
-	<div class="right">
-		<h2>Added</h2>
-		{#each traits.filter((t) => t.done) as todo (todo.id)}
-			<label in:receive={{ key: todo.id }} out:send={{ key: todo.id }} animate:flip>
-				<input type="checkbox" bind:checked={todo.done} />
-				{todo.description}
-			</label>
-		{/each}
-	</div>
+		<div class="right">
+			<h2>Added</h2>
+			{#each traits.filter((t) => t.done) as todo (todo.id)}
+				<label in:receive={{ key: todo.id }} out:send={{ key: todo.id }} animate:flip>
+					<input type="checkbox" bind:checked={todo.done} />
+					{todo.description}
+				</label>
+			{/each}
+		</div>
 	{/if}
 </div>
-
 <div class="absolute-center flex flex-col gap-12 pb-5">
 	<form on:submit={openAiCall} class="flex">
 		<input
@@ -116,35 +118,31 @@
 			bind:value={question}
 			placeholder="Enter your question here"
 		/>
-
 		<button type="submit" class="bg-indigo-800 text-white rounded-r-lg px-10">Summon</button>
 	</form>
-
-	<div class="awnser spook ">
+	<div class="awnser spook">
 		<h1><b>{awnser}</b></h1>
 	</div>
 </div>
 
 <style lang="scss">
-	.spook{
-		color: rgb(55,48,163);
+	.spook {
+		color: rgb(55, 48, 163);
 		font-size: xx-large;
 	}
-	.awnser{
+	.awnser {
 		text-align: center;
 	}
 	.absolute-center {
 		position: absolute;
-		top: 75%;
+		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
 	}
-
 	.board {
 		max-width: 55em;
 		margin: 0 auto;
 	}
-
 	.left,
 	.right {
 		float: left;
@@ -152,13 +150,11 @@
 		padding: 0 1em 0 0;
 		box-sizing: border-box;
 	}
-
 	h2 {
 		font-size: 2em;
 		font-weight: 200;
 		user-select: none;
 	}
-
 	label {
 		top: 0;
 		left: 0;
@@ -171,16 +167,12 @@
 		background-color: #eee;
 		user-select: none;
 	}
-
 	input {
 		margin: 0;
 	}
-
 	.right label {
-		background-color: rgb(55,48,163);
+		background-color: rgb(55, 48, 163);
 		color: white;
+		cursor: pointer;
 	}
-	
-
-
 </style>
