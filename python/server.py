23f4edee3b6a4f02aeaab4540c0da74130db6189
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-23 19:16:54 +0200

    feat - add backend port to env

--- python/server.py
+++ python/server.py
@@ -4,6 +4,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from endpoints.host import host
 from endpoints.join import join
 from endpoints.ai import openai_call
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+load_dotenv()
 
 app = FastAPI()
 
@@ -33,5 +37,5 @@ app.websocket("/join")(join)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('BACKEND_PORT')))
     
