8e9d86dc8c4b53f9ed8562138e0bd2f53781fb74
LANGBRK <kevin.langbroek@hva.nl>
2023-05-24 23:26:48 +0200

    fix: put the testing folder structure back as it should for vitest

---
+++ svelte/src/lib/assets/LobbyCreationPanel.test.ts
@@ -0,0 +1,27 @@
+import { render, fireEvent, screen } from "@testing-library/svelte";
+import LobbyCreationPanel from "$lib/components/LandingPage/LobbyCreationPanel.svelte";
+
+describe("Test LobbyCreationPanel.svelte", async () => {
+	test("Lobby name input is empty when rendered", async () => {
+		// Arrange
+		render(LobbyCreationPanel, { showLobbyCreationPanel: true });
+
+		// Act
+		const input = await screen.getByLabelText("Name your vessel:", { selector: "input" });
+
+		// Assert
+		expect(input).toHaveValue("");
+	});
+
+	test("Lobby name input changes value", async () => {
+		// Arrange
+        render(LobbyCreationPanel, {showLobbyCreationPanel: true});
+
+		// Act
+        const input = await screen.getByLabelText("Name your vessel:", {selector: "input"});
+        await fireEvent.change(input, { target: { value: "Test" }});
+
+		// Assert
+        expect(input).toHaveValue("Test");
+    });
+});
