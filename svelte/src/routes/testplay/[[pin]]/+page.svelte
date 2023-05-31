0512c8c48a3fd3442e672ef78321e5245c69c82d
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-31 17:46:15 +0200

    feat - add /testplay/[[pin]] route and use board component

---
+++ svelte/src/routes/testplay/[[pin]]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import BoardComponent from "$lib/components/BoardComponent.svelte";
+
+	let boardComponent: BoardComponent;
+
+	function onVoteLetter(event: any) {
+		const letterId = event.detail.id;
+		boardComponent.targetALetter(letterId);
+	}
+</script>
+
+<BoardComponent bind:this={boardComponent} on:voteOnLetter={onVoteLetter} />
