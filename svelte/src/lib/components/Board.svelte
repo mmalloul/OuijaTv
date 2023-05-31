4295edc65ca803bb5bd66e124765fe7cb5b625cb
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-31 17:45:10 +0200

    feat - add Board.svelte

---
+++ svelte/src/lib/components/Board.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import BoardSvg from "$lib/components/BoardSVG.svelte";
+    import { onMount, createEventDispatcher } from 'svelte';
+
+
+	const letterPositions: Record<string, Vector2> = {};
+    let seekerPos: Vector2;
+
+    const dispatch = createEventDispatcher();
+
+    onMount(() => {
+        loadLetterPositions();
+    })
+
+    export const targetALetter = (letter: string) => targetLetter(letter)
+
+	function targetLetter(letter: string) {
+		let target = letterPositions[letter.toUpperCase()];
+
+		if (target) {
+			seekerPos = new Vector2(target.x, target.y);
+		}
+	}
+
+    function voteLetter(letterId: string) {
+        dispatch('voteOnLetter', {
+            id: letterId
+        });
+    }
+
+    function loadLetterPositions() {
+		const circleElements = document.querySelectorAll<SVGCircleElement>("circle");
+		circleElements.forEach((element) => {
+			const id = element.id;
+
+			let x = element.attributes.getNamedItem("cx")?.value;
+			let y = element.attributes.getNamedItem("cy")?.value;
+			if (x && y) {
+				letterPositions[id.charAt(id.length - 1)] = new Vector2(parseFloat(x), parseFloat(y));
+			}
+		});
+	}
+
+    class Vector2 {
+		constructor(public x: number, public y: number) {}
+	}
+</script>
+
+<BoardSvg on:click={({ detail: { target } }) => voteLetter(target.id)}>
+    {#if seekerPos && seekerPos.x && seekerPos.y}
+        <circle id="Seeker" cx={seekerPos.x} cy={seekerPos.y} r="76.5" stroke="#FFF7E2" stroke-width="13" />
+    {/if}
+</BoardSvg>
+
+<style lang="postcss">
+	circle {
+		transition: cx 0.5s, cy 0.5s;
+	}
+</style>
