2874dab9692268ec973ca2c60cd2af056889ff30
Kamiel de Visser <kamieldevisser@gmail.com>
2023-06-05 22:16:39 +0200

    feat: add browse page

---
+++ svelte/src/routes/browse/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { env } from "$env/dynamic/public";
+	import { onMount } from "svelte";
+
+	let games: any[] = [];
+
+	onMount(() => {
+		fetchGames();
+	});
+
+	async function fetchGames() {
+		fetch(`${env.PUBLIC_API_URL}/games`)
+			.then((response) => response.json())
+			.then((responseData) => {
+				games = responseData;
+				console.log(games);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	}
+</script>
+
+<table>
+	<thead>
+		<tr>
+			<th>Game Name</th>
+			<th>PIN</th>
+			<th>Join Button</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each games as game}
+            <tr>
+                <td>Lorem Ipsum</td>
+                <td>game.pin</td>
+                <button>hallo</button>
+            </tr>
+		{/each}
+
+	</tbody>
+</table>
