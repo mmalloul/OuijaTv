3c6ae7742f35d8f8167678442f42f8c571519078
LANGBRK <kevin.langbroek@hva.nl>
2023-06-05 03:57:48 +0200

    add toaster comments

--- svelte/src/lib/stores/toast.ts
+++ svelte/src/lib/stores/toast.ts
@@ -2,6 +2,14 @@ import { writable } from "svelte/store";
 import toast, { type ToastPosition } from "svelte-french-toast";
 import { ToastType } from "#lib/types/ToastType";
 
+/**
+ * This store makes it possible to use Toaster in a centralized way, instead of importing the Toaster packages and component separately in each file.
+ * Create a toaster easily with given parameters. 
+ * @param type ToastType -> see ToastType.ts
+ * @param msg message that will be shown.
+ * @param position change position (default: bottom-center)
+ * @param style change style (default: border-radius: 200px; background: #333; color: #fff;)
+ */
 const showToast = (
 	type: ToastType,
 	msg: string,
