1ebaa694bddc92f1b62da356e1c6dffe02b2d04f
Kamiel de Visser <kamieldevisser@gmail.com>
2023-06-05 22:16:24 +0200

    feat: add endpoint /games

--- python/server.py
+++ python/server.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from library import routes
+from library.stores.games import items as games
 
 app = FastAPI()
 
@@ -19,6 +20,22 @@ app.include_router(routes.host)
 app.include_router(routes.join)
 app.include_router(routes.openai)
 
+@app.get("/games")
+def get_games():
+    return [
+        {
+            "pin": key,
+            "players": [
+                {
+                    "name": player.name,
+                    "voted": player.voted,
+                }
+                for player in game.players
+            ]
+            #game.name 
+        }
+        for key, game in games.items()
+    ]
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
