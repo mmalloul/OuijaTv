a6ac81ba8a05b01b05044c170c48511720c6732c
LANGBRK <kevin.langbroek@hva.nl>
2023-06-05 22:53:35 +0200

    feat: add gameMode to LobbyInfo store

--- svelte/src/lib/stores/lobbyStore.ts
+++ svelte/src/lib/stores/lobbyStore.ts
@@ -2,10 +2,12 @@ import { writable } from "svelte/store";
 
 interface LobbyInfo {
   lobbyName: string;
+  gameMode: string;
   gameDuration: number;
 }
 
 export const lobbyStore = writable<LobbyInfo>({
   lobbyName: "",
+  gameMode: "",
   gameDuration: 15,
 });
