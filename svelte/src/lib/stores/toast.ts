4189fc78c50ed01bea20da210f06cbfd7fb16081
LANGBRK <kevin.langbroek@hva.nl>
2023-06-05 03:55:32 +0200

    add  centralized toaster

---
+++ svelte/src/lib/stores/toast.ts
@@ -0,0 +1,28 @@
+import { writable } from "svelte/store";
+import toast, { type ToastPosition } from "svelte-french-toast";
+import { ToastType } from "#lib/types/ToastType";
+
+const showToast = (
+	type: ToastType,
+	msg: string,
+	position = "bottom-center",
+	style = "border-radius: 200px; background: #333; color: #fff;"
+) => {
+	const options = {
+		position: position as ToastPosition,
+		style
+	};
+
+	switch (type) {
+		case ToastType.Success:
+			toast.success(msg, options);
+			break;
+		case ToastType.Error:
+			toast.error(msg, options);
+			break;
+	}
+};
+
+export const toastStore = writable({
+	showToast
+});
