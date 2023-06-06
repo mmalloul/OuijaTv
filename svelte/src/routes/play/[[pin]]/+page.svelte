83e97dc9a1ae32c60ed7984d141ccc3f4fd8a35d
LANGBRK <kevin.langbroek@hva.nl>
2023-06-06 04:44:35 +0200

    feat: game round end features in frontend

--- svelte/src/routes/play/[[pin]]/+page.svelte
+++ svelte/src/routes/play/[[pin]]/+page.svelte
@@ -17,7 +17,9 @@
 	let word: string;
 	let board: Board;
 	let socketController: WebSocketController;
+	let winningVote: string;
 	const playerType: Writable<PlayerType> = getContext("playerType");
+	let canPrompt: boolean;
 
 	let tick: number;
 	let lobbyName: string;
@@ -65,7 +67,10 @@
 	 * Moves the seeker (for host and player) to the targeted letter.
 	 * @param letter the letter to move the seeker to.
 	 */
-	function targetWinningVote(letter: any) {
+	function updateWinningVote(letter: any) {
+		winningVote = letter.detail.winningVote;
+		console.log(winningVote);
+
 		board.moveSeekerToLetter(letter.detail.winningVote);
 	}
 
@@ -79,12 +84,24 @@
 	}
 
 	function restart() {
+		prompt = "";
+		word = "";
 		board.resetSeeker();
+		$toastStore.showToast(ToastType.Success, "Game has been restarted!");
+	}
+
+	function checkAnswer(event: any) {
+		if (winningVote === "!") {
+			$toastStore.showToast(ToastType.Success, "The board has spoken!");
+			prompt = "";
+		} else {
+			updateWord(event);
+		}
+		canPrompt = true;
 	}
 
 	function updateWord(event: any) {
 		word = event.detail.word;
-		prompt = ""; // Reset prompt after a voting round.
 		if ($playerType === PlayerType.Player) {
 			board.allowVoting();
 		}
@@ -102,8 +119,8 @@
 		on:pinReceived={joinGame}
 		on:promptReceived={promptUpdate}
 		on:restartReceived={restart}
-		on:winningVoteReceived={targetWinningVote}
-		on:wordUpdateReceived={updateWord}
+		on:winningVoteReceived={updateWinningVote}
+		on:wordUpdateReceived={checkAnswer}
 		on:tickReceived={updateTick}
 	/>
 
@@ -116,6 +133,7 @@
 				bind:votingTime
 				bind:gameMode
 				bind:prompt
+				bind:canPrompt
 			/>
 		{:else if $playerType === PlayerType.Player}
 			<PlayerController bind:socketController bind:pin bind:prompt />
@@ -148,6 +166,12 @@
 		font-family: theme(fontFamily.amatic);
 	}
 
+	.final-word {
+		@apply text-accent text-10xl;
+		text-decoration: none;
+		font-family: theme(fontFamily.amatic);
+	}
+
 	.voting-timer {
 		@apply opacity-50;
 		border: 1px solid white;
@@ -159,7 +183,7 @@
 	}
 
 	.word {
-		@apply text-fontcolor text-4xl;
+		@apply text-accent text-4xl;
 		text-decoration: none;
 		font-family: theme(fontFamily.amatic);
 	}
