86afc6982fc6550b089fbdc165c9375ca79cd2bd
Malloum003 <mohammed.malloul@hva.nl>
2023-06-07 17:01:03 +0200

    fix: image src not working on live

--- svelte/src/lib/types/Founder.ts
+++ svelte/src/lib/types/Founder.ts
@@ -8,27 +8,27 @@ const founders: Founder[] = [
 	{
 		name: "Mohammed Malloul",
 		studentNumber: "500760524",
-		portrait: "src/lib/assets/images/mohammed.png"
+		portrait: "images/mohammed.png"
 	},
 	{
 		name: "Casper Sluitman",
 		studentNumber: "500854999",
-		portrait: "src/lib/assets/images/casper.png"
+		portrait: "images/casper.png"
 	},
 	{
 		name: "Kamiel de Visser",
 		studentNumber: "500838438",
-		portrait: "src/lib/assets/images/kamiel.png"
+		portrait: "images/kamiel.png"
 	},
 	{
 		name: "Ngoc Ben Nguyen",
 		studentNumber: "500831536",
-		portrait: "src/lib/assets/images/ben.png"
+		portrait: "images/ben.png"
 	},
 	{
 		name: "Kevin Langbroek ",
 		studentNumber: "500847564",
-		portrait: "src/lib/assets/images/kevin.png"
+		portrait: "images/kevin.png"
 	}
 ];
 
