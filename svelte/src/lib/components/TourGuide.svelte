6c6cd17aba7bcf8e55405f7b29bf3f8f9878e7d5
LANGBRK <kevin.langbroek@hva.nl>
2023-06-15 21:30:50 +0200

    feat: add tourguide for lobby-creation-panel

---
+++ svelte/src/lib/components/TourGuide.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import type { IntroJs } from "intro.js";
+
+	let intro: IntroJs;
+
+	onMount(async () => {
+		const { default: IntroJs } = await import("intro.js");
+		await import("intro.js/introjs.css");
+
+		intro = IntroJs();
+	});
+
+	export function startTour() {
+		intro.setOptions({
+            showProgress: true,
+			exitOnEsc: true,
+			overlayOpacity: 0.65,
+
+			steps: [
+				{
+					element: "#lobby-name",
+					intro: "First think of a name for your lobby",
+					position: "right"
+				},
+				{
+					element: "#gameMode",
+					intro: "Choose your gamemode! Either play multiplayer integrated with Twitch or go on a solo adventure against a AI spirit",
+					position: "right"
+				},
+				{
+					element: "#duration",
+					intro: "Decide the amount of voting time for each round!",
+					position: "right"
+				}
+			]
+		});
+
+		intro.start();
+	}
+</script>
