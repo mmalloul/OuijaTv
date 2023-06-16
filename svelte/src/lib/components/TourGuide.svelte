804fd13734f0e7c69a13684678c38f7667b42f7c
Kamiel de Visser <kamieldevisser@gmail.com>
2023-06-16 16:30:36 +0200

    re-write tutorial text

--- svelte/src/lib/components/TourGuide.svelte
+++ svelte/src/lib/components/TourGuide.svelte
@@ -19,18 +19,18 @@
 			steps: [
 				{
 					element: "#lobby-name",
-					intro: "First think of a name for your lobby.",
+					intro: "First, think of a name for your lobby.",
 					position: "right"
 				},
 				{
 					element: "#gameMode",
 					intro:
-						"Choose your game mode! Either play multiplayer integrated with Twitch or go on a solo adventure against a AI spirit.",
+						"Now choose your game mode! Either play multiplayer (compatible with Twitch chat) or go on a solo adventure and ask commune with an AI spirit.",
 					position: "right"
 				},
 				{
 					element: "#duration",
-					intro: "Decide the amount of voting time for each round!",
+					intro: "Finally, set the amount of time between each voting round here.",
 					position: "right"
 				}
 			]
@@ -52,48 +52,48 @@
 				},
 				{
 					element: "#board",
-					intro: "The board shall be used by the spirits to communicate with the host."
+					intro: "This is the board. It shall be used by the spirits to communicate with you."
 				},
 				{
 					element: "#Seeker",
-					intro: "The seeker will move based on the current winning vote."
+					intro: "This is the seeker. It will move towards the character that has the most popular vote among the spirits."
 				},
 				{
 					element: "#prompt",
-					intro: "As a host you decide what to ask to the spirits!",
+					intro: "You decide what to ask the spirits! You can ask them any question, or ask them to spell out a missing word in a sentence.",
 					position: "left"
 				},
 				{
 					element: ".prompt-button",
 					intro:
-						"When you're ready, you can send the question to the spirits. The spirits can only start voting once a question has been asked!",
+						"When you're ready, click on this button to send your question to the spirits. The spirits can only start voting once a question has been asked!",
 					position: "right"
 				},
 				{
 					element: ".timer",
 					intro:
-						"Once the question has been sent the clock will start ticking for each voting round. At the end of each round the most popular letter shall be picked.",
+						"Once the question has been asked, the clock will start ticking. At the end of each round, the most popular letter shall be added to the answer.",
 					position: "left"
 				},
 				{
 					element: ".spirit-answer",
-					intro: "The chosen letters for each round will be displayed here.",
+					intro: "The answer will be built up letter by letter and shown here.",
 					position: "bottom"
 				},
 				{
 					element: ".restart-button",
 					intro:
-						"You can restart the game by pressing this button, which shall reset all the current progress.",
+						"Restart the game by pressing this button. Warning: This resets all the current progress of this game.",
 					position: "right"
 				},
 				{
 					element: ".link-share",
-					intro: "Share this lobby with other players to play with!",
+					intro: "Share this link with others so they can join your lobby as spirits!",
 					position: "left"
 				},
 				{
 					element: "#exit-button",
-					intro: "By exiting the game as a host the lobby shall be closed.",
+					intro: "You can quit the game here. As you are the host, the lobby shall be closed at that point.",
 					position: "right"
 				},
 				{
@@ -119,33 +119,33 @@
 				{
 					element: "#board",
 					intro:
-						"The board shall be used by you and the other spirits to communicate with the host. The seeker will move around based on the votes."
+					"This is the board. It shall be used by the spirits to communicate with you."
 				},
 				{
 					element: "#Seeker",
 					intro:
-						"The bold seeker is the seeker for the game that everybody sees, this will move based on the current winning vote."
+					"This is the seeker. It will move towards the character that has the most popular vote among the spirits."
 				},
 				{
 					element: "#MyVote",
 					intro:
-						"The seeker with a lower opacity is your seeker which will showcase what you voted on. You can only vote once per round!"
+						"The secondary seeker (with a lower opacity) is your personal seeker, it shows you what you personally voted on. You can only vote once per round!"
 				},
 				{
 					element: ".prompt",
 					intro:
-						"The host will ask you a question for you to answer! You can only start voting once a question has been asked!",
+						"The host will ask you a question, it is your task to answer them! You can only start voting once a question has been asked.",
 					position: "left"
 				},
 				{
 					element: ".timer",
 					intro:
-						"Once the question has been sent the clock will start ticking for each voting round. At the end of each round the most popular letter shall be picked.",
+					"Once the question has been asked, the clock will start ticking. At the end of each round, the most popular letter shall be added to the answer.",
 					position: "left"
 				},
 				{
 					element: ".spirit-answer",
-					intro: "The chosen letters for each round will be displayed here.",
+					intro: "The answer will be built up letter by letter and shown here.",
 					position: "bottom"
 				},
 				{
@@ -154,7 +154,7 @@
 					position: "right"
 				},
 				{
-					intro: "Now start communicating with the host as a spirit! 👻"
+					intro: "Now wait for the host to ask you a question and communicate with them as a spirit! 👻"
 				}
 			]
 		});
