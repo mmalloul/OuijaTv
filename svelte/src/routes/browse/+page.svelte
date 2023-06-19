4bc0ff05abe85310d1095299a260fba4b43aa14f
LANGBRK <kevin.langbroek@hva.nl>
2023-06-19 22:11:27 +0200

    feat: add twitch icon if twitch lobby

--- svelte/src/routes/browse/+page.svelte
+++ svelte/src/routes/browse/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import { env } from "$env/dynamic/public";
+	import WebSocketController from "$lib/components/controllers/WebSocketController.svelte";
+	import Icon from "@iconify/svelte";
 	import { onMount } from "svelte";
 
 	let games: any[] = [];
+	let websocket: WebSocketController;
 
 	onMount(() => {
 		fetchGames();
@@ -22,46 +25,69 @@
 </script>
 
 <div class="flex justify-center mt-8 font">
-	<table class="table-auto">
-		<thead>
-			<tr>
-				<th>Game Name</th>
-				<th>PIN</th>
-			</tr>
-		</thead>
-		{#if games.length > 0}
-			<tbody>
-				{#each games as game}
-					<tr>
-						<td>{game.name}</td>
-						<td>{game.pin}</td>
-						<td>
-							<button on:click={() => goto(`/play/${game.pin}`)} class="bg-accent"> Join </button>
-						</td>
-					</tr>
-				{/each}
-			</tbody>
-		{:else}
-			<p>Currently no active vessels... Go conjure one!</p>
-		{/if}
-	</table>
+	<div class="overflow-x-auto">
+		<table class="table-auto w-full">
+			<thead>
+				<tr>
+					<th>Game Name</th>
+					<th>PIN</th>
+					<th>Players</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			{#if games.length > 0}
+				<tbody>
+					{#each games as game}
+						<tr>
+							<td>
+								<div class="flex justify-center items-center">
+									{game.name}
+									{#if game.twitch_channel}
+										<a href={`https://www.twitch.tv/${game.twitch_channel}`}>
+											<Icon icon="mdi:twitch" class="ml-2 twitch-icon" />
+										</a>
+									{/if}
+								</div>
+							</td>
+							<td>{game.pin}</td>
+							<td>{game.players.length}</td>
+							<td>
+								<button on:click={() => goto(`/play/${game.pin}`)} class="join-btn"> Join </button>
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			{:else}
+				<p>Currently no active vessels... Go conjure one!</p>
+			{/if}
+		</table>
+	</div>
 </div>
 
 <style lang="postcss">
+	div :global(.twitch-icon):hover {
+       color: #b9a3e3;
+   }
+
 	.font {
 		@apply text-fontcolor text-4xl;
 		font-family: theme(fontFamily.amatic);
 	}
-	.font th {
-		@apply text-4xl;
-	}
-
-	.font td,
-	.font p {
-		@apply text-xl;
-	}
 
 	td {
 		@apply px-4;
+	}
+
+	table {
+		@apply border-collapse border-2;
+	}
+
+	th,
+	td {
+		@apply px-4 py-2;
+	}
+
+	.join-btn {
+		@apply px-4 py-2 rounded transition duration-200;
 	}
 </style>
