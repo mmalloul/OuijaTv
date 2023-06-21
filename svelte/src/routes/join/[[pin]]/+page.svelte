d11eac294cdc2415781f4a0e65c3c203abb8e8d3
Malloum003 <mohammed.malloul@hva.nl>
2023-06-21 16:07:58 +0200

    feat(lobby panels): polished the lobby join panel

--- svelte/src/routes/join/[[pin]]/+page.svelte
+++ svelte/src/routes/join/[[pin]]/+page.svelte
@@ -4,7 +4,7 @@
 	import { page } from "$app/stores";
 	import { getContext, onMount } from "svelte";
 	import type { Writable } from "svelte/store";
-	import ExitButton from "$lib/components/ExitButton.svelte";
+	import Icon from "@iconify/svelte";
 
 	const username_max = 18;
 	const playerType = getContext<Writable<PlayerType>>("playerType");
@@ -34,54 +34,96 @@
 		playerType.set(PlayerType.Player);
 		goto(`/play/${code}`);
 	}
+
+	function exit() {
+		goto("/");
+	}
 </script>
 
-<div class="page">
-	<ExitButton onExit={(MouseEvent) => Promise.resolve()} />
-	<div class="grow font flex justify-center py-32">
-		<form on:submit|preventDefault class="flex flex-col items-center">
-			<label class="mb-2" for="username">Username:</label>
-			<input
-				class="mb-4"
-				bind:value={username}
-				on:input={validateName}
-				type="text"
-				id="username"
-				name="username"
-			/>
-			<div class="h-4 mb-8 font--error">
-				{validUsername ? "" : "Your username is too long! (Max. 16 characters)"}
+<div class="page container">
+	<div class="panel">
+		<form on:submit|preventDefault class="join-form">
+			<div class="flex flex-col gap-2 w-full relative">
+				<label for="username">Username:</label>
+
+				<button id="close-button" class="close-button" on:click={() => exit()}
+					><Icon icon="zondicons:close-outline" /></button
+				>
+
+				<span class:invisible={validUsername} class="error-message">
+					Your username is too long! (Max. 16 characters)
+				</span>
+
+				<input
+					bind:value={username}
+					on:input={validateName}
+					type="text"
+					id="username"
+					name="username"
+				/>
 			</div>
-			<label class="mb-2" for="username">Roomcode:</label>
-			<input
-				class="mb-4"
-				bind:value={roomCode}
-				on:input={validateRoomCode}
-				type="text"
-				id="room-code"
-				name="room-code"
-			/>
-			<div class="h-4 mb-8 font--error">
-				{validRoomCode ? "" : "Please enter a valid roomcode! (6 characters)"}
+
+			<div class="flex flex-col gap-2 w-full">
+				<label for="username">Roomcode:</label>
+
+				<span class:invisible={validRoomCode} class="error-message">
+					Please enter a valid roomcode! (6 characters)
+				</span>
+
+				<input
+					bind:value={roomCode}
+					on:input={validateRoomCode}
+					type="text"
+					id="room-code"
+					name="room-code"
+				/>
 			</div>
-			<button
-				class="big-button"
-				type="submit"
-				on:click={() => joinRoom(roomCode)}
-				disabled={!validUsername || !validRoomCode}
-				>Join
-			</button>
+
+			<div class="flex gap-2">
+				<button
+					class="big-button"
+					type="submit"
+					on:click={() => joinRoom(roomCode)}
+					disabled={!validUsername || !validRoomCode}
+					>Join
+				</button>
+			</div>
 		</form>
 	</div>
 </div>
 
 <style lang="postcss">
-	.font {
-		@apply text-fontcolor text-4xl;
-		font-family: theme(fontFamily.amatic);
+	.container {
+		@apply flex flex-col items-center mt-15 md: mt-50;
 	}
 
-	.font--error {
-		@apply text-2xl !text-red-500;
+	.panel {
+		@apply flex flex-col items-center justify-center font-amatic text-fontcolor text-center w-full text-lg md:text-4xl p-4;
+		max-width: 500px;
+	}
+
+	input {
+		@apply w-full;
+	}
+
+	.join-form {
+		@apply flex flex-col items-center w-full gap-2 md: gap-4 p-4;
+	}
+
+	.font {
+		@apply text-fontcolor text-4xl font-amatic;
+	}
+
+	.error-message {
+		@apply !text-red-500 text-lg md:text-2xl;
+	}
+
+	.close-button {
+		@apply text-fontcolor absolute top-0 right-0 text-lg md: text-4xl;
+		text-decoration: none;
+	}
+
+	.close-button:hover {
+		@apply text-accent;
 	}
 </style>
